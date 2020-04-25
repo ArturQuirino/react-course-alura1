d93fb6d61e39b5b2243a26b2723835218817294b
Artur Quirino <artur.pquirino@gmail.com>
2020-04-25 15:11:12 -0300

    Adicionando informações no sobre

--- curso-react-front/src/Pages/Sobre/Sobre.js
+++ curso-react-front/src/Pages/Sobre/Sobre.js
@@ -6,6 +6,7 @@ const Sobre = () => {
         <Fragment>
             <Header/>
             <h1>Sobre</h1>
+            <p>Projeto desenvolvido por Artur Quirino durante o curso de React da Alura em 25/04/2020.</p>
         </Fragment>
     );
 }
